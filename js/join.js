71081357436088b9f98346952e4ef3e526e86bc9
christianvionnet <christvionnet@gmail.com>
2022-02-05 19:28:16 -0300

    Cambios en card 2

--- js/join.js
+++ js/join.js
@@ -4,14 +4,13 @@ const card_1 = document.querySelector(".landing__1"),
   planItems1 = document.querySelectorAll("table td:nth-of-type(2)"),
   planItems2 = document.querySelectorAll("table td:nth-of-type(3)"),
   planItems3 = document.querySelectorAll("table td:nth-of-type(4)"),
-  paths = document.querySelectorAll(".landing__tr path"),
-  after = document.querySelectorAll(".after");
+  paths = document.querySelectorAll(".landing__tr path");
 
 //------- CONDICIONES INICIALES --------
 card_1.style.display = "none";
 
-after.forEach((e, i) => {
-  after[i].classList.remove("after");
+planButtons.forEach((e, i) => {
+  planButtons[i].classList.remove("after");
 });
 
 planItems3.forEach((e, i) => {
@@ -19,7 +18,7 @@ planItems3.forEach((e, i) => {
 });
 
 paths[2].style.fill = "var(--red)";
-after[2].classList.add("after");
+planButtons[2].classList.add("after");
 
 planButtons.forEach((e, i) => {
   planButtons[i].addEventListener("click", () => {
@@ -80,49 +79,3 @@ planButtons.forEach((e, i) => {
     }
   });
 });
-
-// planButtons.forEach((e,i) => {
-// })
-
-// planButtons.forEach((e, i) => {
-//   planButtons[i].addEventListener("click", (e) => {
-//     if (!after[i].classList.contains("after")) {
-//       after[i].classList.add("after");
-//     } else {
-//       after.forEach((e, i) => {
-//         after[i].classList.remove("after");
-//       });
-//     }
-//     if (planButtons[i].textContent == "Básico") {
-//       paths[0].style.fill = "var(--red)";
-//       paths[1].style.fill = "var(--light-grey-4)";
-//       paths[2].style.fill = "var(--light-grey-4)";
-
-//       planItems1.forEach((e, i) => {
-//         planItems1[i].style.color = "var(--red)";
-//         planItems2[i].style.color = "var(--light-grey-4)";
-//         planItems3[i].style.color = "var(--light-grey-4)";
-//         planButtons[i].style.opacity = "0.6";
-//       });
-//     } else if (planButtons[i].textContent == "Estándar") {
-//       paths[1].style.fill = "var(--red)";
-//       paths[0].style.fill = "var(--light-grey-4)";
-//       paths[2].style.fill = "var(--light-grey-4)";
-
-//       planItems2.forEach((e, i) => {
-//         planItems1[i].style.color = "var(--light-grey-4)";
-//         planItems2[i].style.color = "var(--red)";
-//         planItems3[i].style.color = "var(--light-grey-4)";
-//       });
-//     } else if (planButtons[i].textContent == "Premium") {
-//       paths[2].style.fill = "var(--red)";
-//       paths[1].style.fill = "var(--light-grey-4)";
-//       paths[0].style.fill = "var(--light-grey-4)";
-//       planItems3.forEach((e, i) => {
-//         planItems1[i].style.color = "var(--light-grey-4)";
-//         planItems2[i].style.color = "var(--light-grey-4)";
-//         planItems3[i].style.color = "var(--red)";
-//       });
-//     }
-//   });
-// });
